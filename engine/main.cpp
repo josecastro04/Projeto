41b2de18059f39011e0292d7ef3115e1d15748ae
Rafaela Antunes <rafaelaantunes151@outlook.pt>
2025-05-08 17:26:01 +0100

    cor

--- engine/main.cpp
+++ engine/main.cpp
@@ -17,30 +17,51 @@
 #include <map>
 
 using namespace std;
+bool solidMode = false;
 static float omega, alpha, radius = 5.0f;
 static float k = 0.5f;
 
-struct Models {
-    std::vector<std::string> model;
+struct Color
+{
+    float diffuse[4] = {1.0f, 1.0f, 1.0f, 1.0f};
+    float ambient[4] = {1.0f, 1.0f, 1.0f, 1.0f};
+    float specular[4] = {1.0f, 1.0f, 1.0f, 1.0f};
+    float emissive[4] = {1.0f, 1.0f, 1.0f, 1.0f};
+    float shininess = 0.0f;
+};
+
+struct Model
+{
+    std::string file;
+    Color color;
+};
+
+struct Models
+{
+    std::vector<Model> model;
     std::vector<Transformation> transformations;
     std::vector<Models> models;
 };
 
-struct Window {
+struct Window
+{
     int width;
     int height;
 };
 
-struct Camera {
+struct Camera
+{
     Point position;
     Point lookAt;
     Point up;
-    struct {
+    struct
+    {
         float fov, near, far;
     } projection;
 };
 
-struct World {
+struct World
+{
     Window window;
     Camera camera;
     Models models;
@@ -55,17 +76,18 @@ struct ModelData
 
 std::map<std::string, ModelData> modelCache;
 
-
-Point calculateVector(Point L, Point P){
+Point calculateVector(Point L, Point P)
+{
     return {L.x - P.x,
             L.y - P.y,
             L.z - P.z};
 }
 
-void SphericalToCartesianFPS() {
+void SphericalToCartesianFPS()
+{
     float direction[3];
     direction[0] = cos(alpha) * cos(omega);
-    direction[1] =  sin(omega);
+    direction[1] = sin(omega);
     direction[2] = sin(alpha) * cos(omega);
 
     normalize(direction);
@@ -73,11 +95,10 @@ void SphericalToCartesianFPS() {
     world.camera.lookAt.x = world.camera.position.x + direction[0];
     world.camera.lookAt.y = world.camera.position.y + direction[1];
     world.camera.lookAt.z = world.camera.position.z + direction[2];
-
 }
-void key_press(unsigned char key, int x, int y) {
-  
-    
+void key_press(unsigned char key, int x, int y)
+{
+
     Point d1 = calculateVector(world.camera.lookAt, world.camera.position);
     float d[3];
     d[0] = d1.x;
@@ -85,24 +106,29 @@ void key_press(unsigned char key, int x, int y) {
     d[2] = d1.z;
     normalize(d);
 
-    if(key == 'e'){
+    if (key == 'e')
+    {
         alpha += 0.02f;
         SphericalToCartesianFPS();
     }
-    if(key == 'q'){
+    if (key == 'q')
+    {
         alpha -= 0.02f;
         SphericalToCartesianFPS();
     }
-    if(key == 'x'){
+    if (key == 'x')
+    {
         omega += 0.02f;
         SphericalToCartesianFPS();
     }
-    if(key == 'z'){
+    if (key == 'z')
+    {
         omega -= 0.02f;
         SphericalToCartesianFPS();
     }
 
-    if(key == 'w'){
+    if (key == 'w')
+    {
         world.camera.lookAt.x += k * d[0];
         world.camera.lookAt.y += k * d[1];
         world.camera.lookAt.z += k * d[2];
@@ -110,7 +136,8 @@ void key_press(unsigned char key, int x, int y) {
         world.camera.position.y += k * d[1];
         world.camera.position.z += k * d[2];
     }
-    if(key == 's'){ 
+    if (key == 's')
+    {
         world.camera.lookAt.x -= k * d[0];
         world.camera.lookAt.y -= k * d[1];
         world.camera.lookAt.z -= k * d[2];
@@ -118,7 +145,8 @@ void key_press(unsigned char key, int x, int y) {
         world.camera.position.y -= k * d[1];
         world.camera.position.z -= k * d[2];
     }
-    if(key == 'a'){
+    if (key == 'a')
+    {
         float right[3];
         float up[3] = {world.camera.up.x, world.camera.up.y, world.camera.up.z}; // (0,1,0) normalmente
         cross(up, d, right);
@@ -130,15 +158,14 @@ void key_press(unsigned char key, int x, int y) {
         world.camera.lookAt.x += k * right[0];
         world.camera.lookAt.y += k * right[1];
         world.camera.lookAt.z += k * right[2];
-      
     }
-    if(key == 'd'){ 
+    if (key == 'd')
+    {
         float right[3];
         float up[3] = {world.camera.up.x, world.camera.up.y, world.camera.up.z}; // (0,1,0) normalmente
         cross(up, d, right);
         normalize(right);
 
-        
         world.camera.position.x -= k * right[0];
         world.camera.position.y -= k * right[1];
         world.camera.position.z -= k * right[2];
@@ -146,20 +173,39 @@ void key_press(unsigned char key, int x, int y) {
         world.camera.lookAt.y -= k * right[1];
         world.camera.lookAt.z -= k * right[2];
     }
-    if(key =='-'){
-        k /=2;
-        if(k < 0.1) k = 0.1;
+    if (key == '-')
+    {
+        k /= 2;
+        if (k < 0.1)
+            k = 0.1;
     }
-    if(key == '+'){
-        k *=2;
-        if(k > 25) k = 25;
+    if (key == '+')
+    {
+        k *= 2;
+        if (k > 25)
+            k = 25;
+    }
+
+    if (key == 't')
+    {
+        
+        solidMode = !solidMode;
+        if (solidMode)
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        }
+        else
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        }
     }
 
     glutPostRedisplay();
-    
 }
 
-void drawAxis() {
+void drawAxis()
+{
+    glDisable(GL_LIGHTING);
     glBegin(GL_LINES);
 
     glColor3f(0.0f, 1.0f, 0.0f);
@@ -176,25 +222,31 @@ void drawAxis() {
 
     glEnd();
     glColor3f(1.0f, 1.0f, 1.0f);
+    glEnable(GL_LIGHTING);
 }
-void parseGroup(tinyxml2::XMLElement *groupElement, Models &models) {
+void parseGroup(tinyxml2::XMLElement *groupElement, Models &models)
+{
     using namespace tinyxml2;
 
-    
     XMLElement *transformElement = groupElement->FirstChildElement("transform");
-    if (transformElement) {
-        for (XMLElement *child = transformElement->FirstChildElement(); child; child = child->NextSiblingElement()) {
+    if (transformElement)
+    {
+        for (XMLElement *child = transformElement->FirstChildElement(); child; child = child->NextSiblingElement())
+        {
             string name_trans = child->Name();
 
-            if (name_trans == "translate") {
-                if (child->Attribute("time")) {
-                    
+            if (name_trans == "translate")
+            {
+                if (child->Attribute("time"))
+                {
+
                     Anime_Translate at;
                     child->QueryFloatAttribute("time", &at.time);
-                    const char* align = child->Attribute("align");
+                    const char *align = child->Attribute("align");
                     at.align = align && string(align) == "true";
 
-                    for (XMLElement *point = child->FirstChildElement("point"); point; point = point->NextSiblingElement("point")) {
+                    for (XMLElement *point = child->FirstChildElement("point"); point; point = point->NextSiblingElement("point"))
+                    {
                         Point p;
                         point->QueryFloatAttribute("x", &p.x);
                         point->QueryFloatAttribute("y", &p.y);
@@ -202,8 +254,10 @@ void parseGroup(tinyxml2::XMLElement *groupElement, Models &models) {
                         at.points.push_back(p);
                     }
                     models.transformations.push_back({at});
-                } else {
-                   
+                }
+                else
+                {
+
                     Translate t;
                     child->QueryFloatAttribute("x", &t.x);
                     child->QueryFloatAttribute("y", &t.y);
@@ -211,17 +265,21 @@ void parseGroup(tinyxml2::XMLElement *groupElement, Models &models) {
                     models.transformations.push_back({t});
                 }
             }
-            else if (name_trans == "rotate") {
-                if (child->Attribute("time")) {
-                    
+            else if (name_trans == "rotate")
+            {
+                if (child->Attribute("time"))
+                {
+
                     Anime_Rotate ar;
                     child->QueryFloatAttribute("time", &ar.time);
                     child->QueryFloatAttribute("x", &ar.x);
                     child->QueryFloatAttribute("y", &ar.y);
                     child->QueryFloatAttribute("z", &ar.z);
                     models.transformations.push_back({ar});
-                } else {
-                    
+                }
+                else
+                {
+
                     Rotate r;
                     child->QueryFloatAttribute("angle", &r.angle);
                     child->QueryFloatAttribute("x", &r.x);
@@ -230,7 +288,8 @@ void parseGroup(tinyxml2::XMLElement *groupElement, Models &models) {
                     models.transformations.push_back({r});
                 }
             }
-            else if (name_trans == "scale") {
+            else if (name_trans == "scale")
+            {
                 Scale s;
                 child->QueryFloatAttribute("x", &s.x);
                 child->QueryFloatAttribute("y", &s.y);
@@ -240,89 +299,137 @@ void parseGroup(tinyxml2::XMLElement *groupElement, Models &models) {
         }
     }
 
-    
     XMLElement *modelsElement = groupElement->FirstChildElement("models");
-    if (modelsElement) {
-        for (XMLElement *model = modelsElement->FirstChildElement("model"); model; model = model->NextSiblingElement("model")) {
+    if (modelsElement)
+    {
+        for (XMLElement *model = modelsElement->FirstChildElement("model"); model; model = model->NextSiblingElement("model"))
+        {
+            Model m;
+
             const char *file = model->Attribute("file");
-            if (file) models.model.push_back(file);
+            if (file)
+                m.file = file;
+
+            XMLElement *color = model->FirstChildElement("color");
+            if (color)
+            {
+                XMLElement *diffuse = color->FirstChildElement("diffuse");
+                if (diffuse)
+                    diffuse->QueryFloatAttribute("R", &m.color.diffuse[0]),
+                    diffuse->QueryFloatAttribute("G", &m.color.diffuse[1]),
+                    diffuse->QueryFloatAttribute("B", &m.color.diffuse[2]);
+
+                XMLElement *ambient = color->FirstChildElement("ambient");
+                if (ambient)
+                    ambient->QueryFloatAttribute("R", &m.color.ambient[0]),
+                    ambient->QueryFloatAttribute("G", &m.color.ambient[1]),
+                    ambient->QueryFloatAttribute("B", &m.color.ambient[2]);
+
+                XMLElement *specular = color->FirstChildElement("specular");
+                if (specular)
+                    specular->QueryFloatAttribute("R", &m.color.specular[0]),
+                    specular->QueryFloatAttribute("G", &m.color.specular[1]),
+                    specular->QueryFloatAttribute("B", &m.color.specular[2]);
+
+                XMLElement *emissive = color->FirstChildElement("emissive");
+                if (emissive)
+                    emissive->QueryFloatAttribute("R", &m.color.emissive[0]),
+                    emissive->QueryFloatAttribute("G", &m.color.emissive[1]),
+                    emissive->QueryFloatAttribute("B", &m.color.emissive[2]);
+
+                XMLElement *shininess = color->FirstChildElement("shininess");
+                if (shininess)
+                    shininess->QueryFloatAttribute("value", &m.color.shininess);
+            }
+
+            models.model.push_back(m); 
         }
+        
     }
 
-
-    for (XMLElement *childGroup = groupElement->FirstChildElement("group"); childGroup; childGroup = childGroup->NextSiblingElement("group")) {
+    for (XMLElement *childGroup = groupElement->FirstChildElement("group"); childGroup; childGroup = childGroup->NextSiblingElement("group"))
+    {
         Models childModels;
         parseGroup(childGroup, childModels);
         models.models.push_back(childModels);
     }
 }
 
-void parseInfo(char *filename) {
+void parseInfo(char *filename)
+{
     using namespace tinyxml2;
     XMLDocument doc;
-    if (doc.LoadFile(filename) != XML_SUCCESS) {
+    if (doc.LoadFile(filename) != XML_SUCCESS)
+    {
         cerr << "Error loading XML file" << endl;
         exit(1);
     }
 
     XMLNode *pRoot = doc.FirstChild();
-    if (!pRoot) {
+    if (!pRoot)
+    {
         cerr << "Empty XML file" << endl;
         exit(1);
     }
 
-    
     XMLElement *window = pRoot->FirstChildElement("window");
-    if (window) {
+    if (window)
+    {
         window->QueryIntAttribute("width", &world.window.width);
         window->QueryIntAttribute("height", &world.window.height);
     }
 
-    
     XMLElement *camera = pRoot->FirstChildElement("camera");
-    if (camera) {
+    if (camera)
+    {
         XMLElement *pos = camera->FirstChildElement("position");
-        if (pos) {
+        if (pos)
+        {
             pos->QueryFloatAttribute("x", &world.camera.position.x);
             pos->QueryFloatAttribute("y", &world.camera.position.y);
             pos->QueryFloatAttribute("z", &world.camera.position.z);
         }
 
         XMLElement *lookAt = camera->FirstChildElement("lookAt");
-        if (lookAt) {
+        if (lookAt)
+        {
             lookAt->QueryFloatAttribute("x", &world.camera.lookAt.x);
             lookAt->QueryFloatAttribute("y", &world.camera.lookAt.y);
             lookAt->QueryFloatAttribute("z", &world.camera.lookAt.z);
         }
 
         XMLElement *up = camera->FirstChildElement("up");
-        if (up) {
+        if (up)
+        {
             up->QueryFloatAttribute("x", &world.camera.up.x);
             up->QueryFloatAttribute("y", &world.camera.up.y);
             up->QueryFloatAttribute("z", &world.camera.up.z);
         }
 
         XMLElement *proj = camera->FirstChildElement("projection");
-        if (proj) {
+        if (proj)
+        {
             proj->QueryFloatAttribute("fov", &world.camera.projection.fov);
             proj->QueryFloatAttribute("near", &world.camera.projection.near);
             proj->QueryFloatAttribute("far", &world.camera.projection.far);
         }
     }
 
-    
-    radius = sqrt(pow(world.camera.lookAt.x - world.camera.position.x, 2) + 
-              pow(world.camera.lookAt.y - world.camera.position.y, 2) + 
-              pow(world.camera.lookAt.z - world.camera.position.z, 2));
+    radius = sqrt(pow(world.camera.lookAt.x - world.camera.position.x, 2) +
+                  pow(world.camera.lookAt.y - world.camera.position.y, 2) +
+                  pow(world.camera.lookAt.z - world.camera.position.z, 2));
     omega = asin((world.camera.lookAt.y - world.camera.position.y) / radius);
-    alpha = atan2(world.camera.lookAt.z - world.camera.position.z, 
-        world.camera.lookAt.x - world.camera.position.x); 
+    alpha = atan2(world.camera.lookAt.z - world.camera.position.z,
+                  world.camera.lookAt.x - world.camera.position.x);
 
+                  
     XMLElement *group = pRoot->FirstChildElement("group");
-    if (group) parseGroup(group, world.models);
+    if (group)
+        parseGroup(group, world.models);
 }
 
-void drawFigureVBO(string filename) {
+void drawFigureVBO(string filename)
+{
     if (modelCache.find(filename) == modelCache.end())
     {
 
@@ -378,7 +485,6 @@ void drawFigureVBO(string filename) {
         glBindBuffer(GL_ARRAY_BUFFER, buffers[1]);
         glBufferData(GL_ARRAY_BUFFER, sizeof(float) * num_vertices * 3, n, GL_STATIC_DRAW);
 
-        
         free(v);
         free(n);
 
@@ -406,67 +512,86 @@ void drawFigureVBO(string filename) {
     glDisableClientState(GL_VERTEX_ARRAY);
 }
 
-
-void drawModel(Models& models) {
-    
-    for (const auto& transformation : models.transformations) {
-        if (holds_alternative<Anime_Translate>(transformation.type)) {
-            const auto& trans = get<Anime_Translate>(transformation.type);
+void drawModel(Models &models)
+{
+    glPushMatrix();
+    for (const auto &transformation : models.transformations)
+    {
+        if (holds_alternative<Anime_Translate>(transformation.type))
+        {
+            const auto &trans = get<Anime_Translate>(transformation.type);
             drawCatmullRomCurve(trans.points);
         }
     }
 
-    glPushMatrix();
-
-    
-    for (auto& transformation : models.transformations) {
+    for (auto &transformation : models.transformations)
+    {
         applyTransformation(transformation);
     }
 
-   
-    for (const string& filename : models.model) {
-        glPushMatrix();
-        drawFigureVBO(filename);
-        glPopMatrix();
+    for (const Model &m : models.model)
+    {
+
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, m.color.diffuse);
+        glMaterialfv(GL_FRONT, GL_AMBIENT, m.color.ambient);
+        glMaterialfv(GL_FRONT, GL_SPECULAR, m.color.specular);
+        glMaterialfv(GL_FRONT, GL_EMISSION, m.color.emissive);
+        glMaterialf(GL_FRONT, GL_SHININESS, m.color.shininess);
+
+        drawFigureVBO(m.file);
     }
 
-   
-    for (Models& child : models.models) {
+    for (Models &child : models.models)
+    {
         drawModel(child);
     }
 
     glPopMatrix();
 }
 
-void changeSize(int w, int h) {
-    if (h == 0) h = 1;
+void changeSize(int w, int h)
+{
+    if (h == 0)
+        h = 1;
     float ratio = w * 1.0 / h;
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glViewport(0, 0, w, h);
-    gluPerspective(world.camera.projection.fov, ratio, 
-                  world.camera.projection.near, world.camera.projection.far);
+    gluPerspective(world.camera.projection.fov, ratio,
+                   world.camera.projection.near, world.camera.projection.far);
     glMatrixMode(GL_MODELVIEW);
 }
 
-void renderScene() {
+void renderScene()
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glLoadIdentity();
     gluLookAt(world.camera.position.x, world.camera.position.y, world.camera.position.z,
               world.camera.lookAt.x, world.camera.lookAt.y, world.camera.lookAt.z,
               world.camera.up.x, world.camera.up.y, world.camera.up.z);
-
-    glPolygonMode(GL_FRONT, GL_LINE);
     drawAxis();
+    if (solidMode)
+    {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
+    else
+    {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    }
+   
+
     drawModel(world.models);
-    
+
     glutSwapBuffers();
 }
 
-int main(int argc, char **argv) {
-    if (argc < 2) {
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
         cerr << "Usage: " << argv[0] << " <scene_file.xml>" << endl;
         return 1;
     }
@@ -485,15 +610,18 @@ int main(int argc, char **argv) {
     glutReshapeFunc(changeSize);
     glutKeyboardFunc(key_press);
     glutIdleFunc(renderScene);
-    
 
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
-    glPolygonMode(GL_FRONT, GL_LINE);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
     glutMainLoop();
 
-    for(auto &entry : modelCache) {
+    for (auto &entry : modelCache)
+    {
         glDeleteBuffers(2, entry.second.vbo);
     }
 
